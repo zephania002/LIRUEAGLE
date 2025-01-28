b63ca19997c4b8ccc18581f6608b4981a57a1aeb
zephany002 <zephany001@gmail.com>
2025-01-28 23:46:36 +0300

    timer initialize

--- assets/js/Scroll.js
+++ assets/js/Scroll.js
@@ -1,6 +1,6 @@
 // Smooth Scroll on Clicking the Scroll Down Button
 document.getElementById('scroll-down').addEventListener('click', function () {
-    const nextSection = document.querySelector('#work'); // Change to the ID of your next section
+    const nextSection = document.querySelector('#products'); // Change to the ID of your next section
     if (nextSection) {
         nextSection.scrollIntoView({ behavior: 'smooth' });
     }
